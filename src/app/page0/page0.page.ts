01163d31210bb7ac8d91cfa6a1aa90740505d95b
ArthurCoutinho48 <165350241+ArthurCoutinho48@users.noreply.github.com>
2025-05-09 15:45:58 -0300

    ukt

---
+++ src/app/page0/page0.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page0',
+  templateUrl: './page0.page.html',
+  styleUrls: ['./page0.page.scss'],
+  standalone: false,
+})
+export class Page0Page implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
